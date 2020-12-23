4728b165ede37e98dfa4c7cbdd3017f83190665f
ycp19940225 <820363773@qq.com>
2020-12-23 15:03:20 +0800

    init

---
+++ src/test/Test.php
@@ -0,0 +1,17 @@
+<?php
+namespace Yangcp\test;
+
+
+/**
+ * User: YCP
+ * Date: 2020/12/23
+ * Class Test
+ * @package Yangcp\test
+ */
+class Test{
+
+    public function index()
+    {
+        echo 'ycp, welcome!';
+    }
+}
